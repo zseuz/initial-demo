52191d1b4b7d91200fbef73361773939db813f25
jonathan773 <jonathanramir52@gmail.com>
2022-09-17 07:14:56 -0500

    first commit

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,22 @@
+import { Box } from "@mui/system";
+import Head from "next/head";
+import { FC } from "react";
+import { Navbar, Sidebar } from "../ui";
+
+interface Props {
+  title?: string;
+}
+
+export const Layout: FC<Props> = ({ title = "OpenJira", children }) => {
+  return (
+    <Box sx={{ flexFlow: 1 }}>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Navbar />
+      <Sidebar/>
+
+      <Box sx={{ padding: "10px 20px" }}>{children}</Box>
+    </Box>
+  );
+};
---
+++ components/ui/Sidebar.tsx
@@ -0,0 +1,62 @@
+import { useContext } from "react";
+import {
+  Box,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+
+import InboxOutlinedIcon from "@mui/icons-material/InboxOutlined";
+import MailOutlineOutlinedIcon from "@mui/icons-material/MailOutlineOutlined";
+
+
+const menuItems: string[] = ["Inbox", "Starred", "Send Email", "Drafts"];
+
+export const Sidebar = () => {
+
+  return (
+    <Drawer anchor="left" open={true} onClose={}>
+      <Box sx={{ width: 250 }}>
+        <Box sx={{ padding: "5px 10px" }}>
+          <Typography variant="h4">Menú</Typography>
+        </Box>
+
+        <List>
+          {menuItems.map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 ? (
+                  <InboxOutlinedIcon />
+                ) : (
+                  <MailOutlineOutlinedIcon />
+                )}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+
+        <Divider />
+
+        <List>
+          {menuItems.map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 ? (
+                  <InboxOutlinedIcon />
+                ) : (
+                  <MailOutlineOutlinedIcon />
+                )}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Drawer>
+  );
+};
